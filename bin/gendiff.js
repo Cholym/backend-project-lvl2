ffff061503244d3630fee27da37be6101fc6d1e9
Olga Chabanova <troplu381@gmail.com>
2022-07-07 15:53:41 +0300

    Step 2 Аргументы и опции

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,14 @@ import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
 // Commands
+
+
+// Options
 program
 .description('Compares two configuration files and shows a difference.')
 .version('0.0.1')
+.option('-f, --format <type>', 'output format')
+.argument('<filepath1>')
+.argument('<filepath2>')
 
-program.parse();
+program.parse(process.argv);
